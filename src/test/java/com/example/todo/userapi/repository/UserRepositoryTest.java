c0f7b4657b58baddc7ec6003e1b713aaef667e0d
changju <changju7857@gmail.com>
2023-06-21 18:08:43 +0900

    # 3. 시큐리티모듈추가, user 엔터티 및 테스트 실행

---
+++ src/test/java/com/example/todo/userapi/repository/UserRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.example.todo.userapi.repository;
+
+import com.example.todo.userapi.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    void saveTest()
+    {
+        //given
+        User newUser = User.builder()
+                .email("abc1234@abc.com")
+                .password("1234")
+                .userName("춘식이")
+                .build();
+        //when
+        User saved = userRepository.save(newUser);
+
+        //then
+        assertNotNull(saved);
+    }
+
+    @Test
+    @DisplayName("이메일로 회원 조회하기")
+    void findEmailTest()
+    {
+        //given
+        String email = "abc1234@abc.com";
+        //when
+        Optional<User> userOptional = userRepository.findByEmail(email);
+
+        //then
+        assertTrue(userOptional.isPresent()); // 실존한다고 단언한다.
+        User user = userOptional.get();
+        assertEquals("춘식이", user.getUserName()); // user에 이름이 춘식이랑 같을 것이다
+
+        System.out.println("\n\n\n");
+        System.out.println("user = " + user);
+        System.out.println("\n\n\n");
+    }
+
+    @Test
+    @DisplayName("이메일 중복체크를 하면 중복값이 false여야 한다")
+    void emailFalse()
+    {
+        //given
+        String email = "db1234@abc.com";
+        //when
+        boolean flag = userRepository.existsByEmail(email);
+        //then
+        assertFalse(flag); // 이것이 false 일거라고 단언한다.
+    }
+
+
+}
