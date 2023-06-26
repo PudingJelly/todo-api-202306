40b9e1cf12d61d6f2cec960ecc21b27242998f7f
changju <changju7857@gmail.com>
2023-06-26 13:13:53 +0900

    # 5. 토큰 인증 처리

--- src/main/java/com/example/todo/config/WebSecurityConfig.java
+++ src/main/java/com/example/todo/config/WebSecurityConfig.java
@@ -1,16 +1,25 @@
 package com.example.todo.config;
 
+import com.example.todo.filter.JwtAuthFilter;
+import com.example.todo.userapi.entity.Role;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 //@Configuration // 설정 클래스 용도로 사용하도록 스프링에 등록하는 어노테이션
 @EnableWebSecurity // 시큐리티 설정 파일로 사용할 클래스 선언.
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
 
     @Bean // 외부 라이브러리를 사용하기 위해 스프링에 등록 해주는 어노테이션
@@ -26,7 +35,22 @@ public class WebSecurityConfig {
                 .cors()
                 .and()
                 .csrf().disable()
-                .httpBasic().disable();
+                .httpBasic().disable()
+                // 세션 인증을 사용하지 않겠다라는 설정
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // 어떤 요청에서 인증을 안 할 것인지 설정, 언제 할 것인지 설정
+                .authorizeRequests()
+                .antMatchers("/", "/api/auth/**").permitAll()
+//                .antMatchers(HttpMethod.POST, "/api/auth/todos").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        // 토큰 인증 필터 연결
+        http.addFilterAfter(
+                jwtAuthFilter,
+                CorsFilter.class // import 주의 : spring거로 가져올것
+        );
 
         return http.build();
 
